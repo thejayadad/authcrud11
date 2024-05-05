e2670c74821266fdd059ca4f985b8b859d812c6e
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-05-05 15:25:34 -0400

    search-commit

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,64 +1,69 @@
-import Box from '@/components/box'
-import CreateButton from '@/components/buttons/buttons'
-import PlayerCard from '@/components/player-card/player-card'
-import Search from '@/components/search'
-import Site from '@/components/site'
-import prisma from '@/lib/prisma'
-import { Skeleton } from '@nextui-org/react'
-import Link from 'next/link'
-import React, { Suspense } from 'react'
+import React from 'react';
+import Box from '@/components/box';
+import CreateButton from '@/components/buttons/buttons';
+import PlayerCard from '@/components/player-card/player-card';
+import Search from '@/components/search';
+import Site from '@/components/site';
+import prisma from '@/lib/prisma';
+import { Skeleton } from '@nextui-org/react';
 
-const HomePage = async () => {
+const HomePage = ({ searchParams }) => {
+  const query = searchParams?.query || "";
   return (
-    <section className='p-6'>
+    <section className="p-6">
       <Box>
         <Site />
-        <div className='flex items-center gap-2'>
-        <Search />
-        <CreateButton />
+        <div className="flex items-center gap-2">
+          <Search />
+          <CreateButton />
         </div>
-       <div className='grid grid-cols-2 md:grid-cols-2   mt-4 gap-4'>
-       <Suspense fallback={<PlayerListFallback />}>
-       <PlayerList />
-       </Suspense>
-       </div>
+        <div className="grid grid-cols-2 md:grid-cols-2 mt-4 gap-4">
+          <PlayerList query={query} />
+        </div>
       </Box>
-    </section> 
-  )
-}
+    </section>
+  );
+};
 
-async function PlayerList(){
-  const collections = await prisma.player.findMany({});
-  if(collections.length === 0){ // Check the length of the collections array
+async function PlayerList({ query }) {
+  let collections;
+  if (query) {
+    collections = await prisma.player.findMany({
+      where: {
+        name: {
+          contains: query,
+          mode: 'insensitive', // Case-insensitive search
+        },
+      },
+    });
+  } else {
+    collections = await prisma.player.findMany({});
+  }
+
+  if (collections.length === 0) {
     return (
-      <div className='text-center col-span-4 mt-4'>
-        <div className='flex flex-col space-y-1'>
-          <h2 className='font-bold'>No Players Added</h2>
-          <p className='font-light'>Start tracking today!</p>
+      <div className="text-center col-span-4 mt-4">
+        <div className="flex flex-col space-y-1">
+          <h2 className="font-bold">No Players Added</h2>
+          <p className="font-light">Start tracking today!</p>
         </div>
       </div>
     );
   }
-  return (
-    <>
-    {collections.map(collection => (
-      <PlayerCard
-        key={collection.id}
-        collection={collection}
-      />
-    ))}
-    </>
-  );
+
+  return collections.map((collection) => (
+    <PlayerCard key={collection.id} collection={collection} />
+  ));
 }
 
-function PlayerListFallback(){
+function PlayerListFallback() {
   return (
-    <div className='p-4'>
-        <Skeleton className="rounded-lg">
+    <div className="p-4">
+      <Skeleton className="rounded-lg">
         <div className="p-4 h-full rounded-lg bg-default-300"></div>
       </Skeleton>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- components/search.js
+++ components/search.js
@@ -1,13 +1,29 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 const Search = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get('query') || '');
+
+  const handleSearch = (event) => {
+    const newQuery = event.target.value.trim();
+    setQuery(newQuery);
+    const params = new URLSearchParams(searchParams);
+    params.set('query', newQuery);
+    router.replace(`/?${params.toString()}`);
+  };
+
   return (
     <div className="relative pt-4 w-full">
       <input
         type="text"
         placeholder="Search..."
         className="w-full py-2 pl-10 pr-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+        onChange={handleSearch}
+        value={query}
       />
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <FiSearch className="text-gray-400 mt-4" />
